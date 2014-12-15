bf2c1c9f8fd60cbacf410484cf73ecf452f91a88
Adam Bodie <adam.bodie@yahoo.com>
2014-12-14 19:55:54 -0800

    Add index and show views

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,20 @@
 class ItemsController < ApplicationController
-  def new
-    
+  def new    
   end
   def create
-    @item = Item.new(article_params)
+    @item = Item.new(item_params)
  
-  @item.save
-  redirect_to @item
-end
- 
+    @item.save
+    redirect_to @item
+  end
+  def show
+    @item = Item.find(params[:id])
+  end
+  
+  def index
+    @items = Item.all
+  end 
+  
 private
   def item_params
     params.require(:item).permit(:title, :size, :description, :price)
