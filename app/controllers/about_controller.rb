3769143afdec81e608bf44126e10aa59a8f4fdb4
Adam Bodie <adam.bodie@yahoo.com>
2014-12-29 16:13:19 -0800

    Make ads appear randomly through controllers

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,4 +1,11 @@
 class AboutController < ApplicationController
   def index
+    @horizontal_ad = ["Boring", "Viagra", "Human"]
+    horizontal_number = (@horizontal_ad.length * rand).to_i
+    @horizontal_name = @horizontal_ad[horizontal_number]
+    
+    @vertical_ad = ["GOP", "Money", "Starbucks"]
+    vertical_number = (@vertical_ad.length * rand).to_i
+    @vertical_name = @vertical_ad[vertical_number]
   end
 end
